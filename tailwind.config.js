b882fbfa6264259bf85711896a7ee1c067b70835
aminulhasan142 <mahshoukhin142@gmail.com>
2024-03-07 08:58:38 +0600

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px"
+    },
+    extend: {
+      colors: {
+        colorNeutral400: "#A3A3A3",
+        colorNeutral950: "#0A0A0A",
+        colorNeutral700: ""
+      },
+    },
   },
   plugins: [],
 }
